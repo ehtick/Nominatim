9e5152edf44d1a5339e355bdea094992cb978730
Sarah Hoffmann <lonvia@denofr.de>
2026-08-25 19:43:47 +0200

    use knn to find nearest street and place

--- lib-sql/functions/partition-functions.sql
+++ lib-sql/functions/partition-functions.sql
@@ -161,12 +161,13 @@ BEGIN
 
 {% for partition in db.partitions %}
   IF in_partition = {{ partition }} THEN
-    SELECT place_id FROM search_name_{{ partition }}
+    SELECT CASE WHEN token_matches_street(token_info, name_vector) THEN place_id ELSE NULL END
       INTO parent
-      WHERE token_matches_street(token_info, name_vector)
-            AND centroid && ST_Expand(point, 0.015)
+      FROM search_name_{{ partition }}
+      WHERE (token_matches_street(token_info, name_vector)
+             OR ST_Distance(centroid, point) > 0.1)
             AND address_rank between 26 and 27
-      ORDER BY ST_Distance(centroid, point) ASC limit 1;
+      ORDER BY centroid <-> point LIMIT 1;
     RETURN parent;
   END IF;
 {% endfor %}
@@ -190,13 +191,13 @@ BEGIN
 
 {% for partition in db.partitions %}
   IF in_partition = {{ partition }} THEN
-    SELECT place_id
+    SELECT CASE WHEN token_matches_place(token_info, name_vector) THEN place_id ELSE NULL END
       INTO parent
       FROM search_name_{{ partition }}
-      WHERE token_matches_place(token_info, name_vector)
-            AND centroid && ST_Expand(point, 0.04)
+      WHERE (token_matches_place(token_info, name_vector)
+             OR ST_Distance(centroid, point) > 0.1)
             AND address_rank between 16 and 25
-      ORDER BY ST_Distance(centroid, point) ASC limit 1;
+      ORDER BY centroid <-> point LIMIT 1;
     RETURN parent;
   END IF;
 {% endfor %}
